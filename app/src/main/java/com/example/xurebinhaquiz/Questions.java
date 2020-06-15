e3bbf621a35e5141e4e4baabaedb458d039a5c5a
FelipeWurch <felipe.wurch@neogrid.com>
2020-06-15 15:19:57 -0300

    commitando na master fodase

--- app/src/main/java/com/example/xurebinhaquiz/Questions.java
+++ app/src/main/java/com/example/xurebinhaquiz/Questions.java
@@ -5,89 +5,88 @@ import java.util.List;
 
 public class Questions {
 
-    private int id;
     private String pergunta;
 
-    List<Questions> questionsList = new ArrayList<Questions>(){
-        {
-            add(new Questions(1,"Sou bastante autêntico e digo aquilo que penso."));
-            add(new Questions(2,"Sou introspectivo, penso muito antes de me expor."));
-            add(new Questions(3,"Questões intelectuais profundas não me interessam."));
-            add(new Questions(4,"Sou persistente e não desisto até atingir meus objetivos."));
-            add(new Questions(5,"As pessoas podem contar comigo. Sou bastante estável em minhas ações. "));
-            add(new Questions(6,"Gosto de trabalhar com materiais, com as mãos, objetos e instrumentos. "));
-            add(new Questions(7,"Consigo aguentar por um bom tempo uma atividade física ou intelectual. "));
-            add(new Questions(8,"Dou muito valor para o dinheiro."));
-            add(new Questions(9,"Sei utilizar instrumentos e aparelhos eletrônicos."));
-            add(new Questions(10,"Aceito bem regras, limites, ordens e o contexto no qual vivo."));
+    List<String> questionsList = new ArrayList<>();
 
-            add(new Questions(11,"Gosto de observar, analisar e entender o modo como as coisas, a natureza ou as pessoas funcionam. "));
-            add(new Questions(12,"Não sou muito impulsivo, penso antes de agir."));
-            add(new Questions(13,"Áreas como Matemática e Ciências me atraem. Gosto de estudar um assunto a fundo."));
-            add(new Questions(14,"Tenho facilidade para aprender."));
-            add(new Questions(15,"Sou racional para tomar uma decisão ou para decidir o melhor caminho a seguir."));
-            add(new Questions(16,"Sou muito organizado e detalhista."));
-            add(new Questions(17,"Sou capaz de dar respostas precisas sobre datas, valores e descrever bem determinada situação do passado."));
-            add(new Questions(18,"Tenho um grande senso crítico. As coisas sempre podem ser melhoradas."));
-            add(new Questions(19,"Gosto de tomar minhas próprias decisões, mesmo que sejam diferentes do pensamento da maioria. "));
-            add(new Questions(20,"Tenho boa habilidade lógica ou matemática."));
 
-            add(new Questions(21,"É difícil para mim aceitar regras, limites e algo que me é imposto."));
-            add(new Questions(22,"Para mim, é fácil viver em um ambiente desorganizado e fazer as coisas sem uma lógica. "));
-            add(new Questions(23,"Sou muito sensível. Acredito que possamos construir um mundo melhor."));
-            add(new Questions(24,"Sou uma pessoa idealista. Acredito que possamos construir um mundo melhor."));
-            add(new Questions(25,"Gosto de criar, inventar, idealizar e fantasiar."));
-            add(new Questions(26,"Gosto de causar impacto, provocando, entretendo, seduzindo, encantando as pessoas. "));
-            add(new Questions(27,"Gosto de descobrir novas possibilidades para as coisas e situações da vida."));
-            add(new Questions(28,"Muitas vezes, não penso muito antes de tomar uma decisão, nem meço as consequências."));
-            add(new Questions(29,"Gosto de me expressar, de representar ou de me mostrar em público. "));
-            add(new Questions(30,"Responsabilizo-me por minhas atitudes e decisões."));
 
-            add(new Questions(31,"Gosto de liderar, de organizar ou de mobilizar um grupo em torno de uma causa ou ação. "));
-            add(new Questions(32,"Gosto de ajudar e colaborar com os outros. Sou leal e benevolente. "));
-            add(new Questions(33,"Uso a emoção e a sensibilidade para tomar decisões."));
-            add(new Questions(34,"Em geral, sou uma pessoa agradável e fácil de lidar."));
-            add(new Questions(35,"Sou idealista e quero mais da vida."));
-            add(new Questions(36,"Questões éticas, de justiça ou bem-estar comum são importantes para mim. "));
-            add(new Questions(37,"Sou responsável. Podem contar comigo porque sempre correspondo ao esperado."));
-            add(new Questions(38,"Costumo me dedicar a pessoas ou a causas, “vestindo a camisa” de uma entidade, de um grupo ou de um clube que eu acredite."));
-            add(new Questions(39,"No geral, me relaciono bem com os outros. Tenho facilidade para dialogar."));
-            add(new Questions(40,"Gosto de ajudar, ensinar, curar e, de alguma forma, quero ser importante para os outros."));
 
-            add(new Questions(41,"Sou ﬁrme e conﬁante em minhas decisões e não preciso de palpites. "));
-            add(new Questions(42,"Muitas vezes consigo convencer as pessoas sobre determinado ponto de vista. "));
-            add(new Questions(43,"Sou uma pessoa motivada, incentivada, otimista."));
-            add(new Questions(44,"Falo muito. Não desisto facilmente de uma argumentação."));
-            add(new Questions(45,"Dou muito valor aos bens materiais. Gosto de comprar e, às vezes, até de demonstrar aos outros minhas conquistas, meus bens materiais, minha sabedoria e meus talentos."));
-            add(new Questions(46,"Tomo decisões rapidamente. Ajo sem reﬂetir muito. Chego a ser impulsivo."));
-            add(new Questions(47,"Gosto de estar no meio das pessoas, especialmente para mostrar a elas o modo como penso."));
-            add(new Questions(48,"Gosto muito de aventuras, de me arriscar e de desaﬁos."));
-            add(new Questions(49,"Estou sempre procurando novas oportunidades, novos caminhos a seguir."));
-            add(new Questions(50,"Preﬁro ser o dono de um negócio do que trabalhar para alguém. "));
-
-            add(new Questions(51,"Aceito bem as regras e o contexto no qual vivo. Gosto de limites claros."));
-            add(new Questions(52,"Sou introvertido, me resguardo e não me exponho muito."));
-            add(new Questions(53,"Cuido de minhas coisas e gosto de conservá-las."));
-            add(new Questions(54,"Sou bem calmo e tenho bom equilíbrio emocional."));
-            add(new Questions(55,"Preﬁro ter um plano de ação, uma rotina deﬁnida de trabalho."));
-            add(new Questions(56,"Sou um tipo de pessoa estável, conﬁável, adequada à realidade."));
-            add(new Questions(57,"Respeito a ética, guardo segredos, cumpro com o combinado."));
-            add(new Questions(58,"Sou muito organizado e detalhista nas coisas. Eﬁcácia é muito importante para mim."));
-            add(new Questions(59,"Eu vou até o ﬁm em meus objetivos. Não desisto até atingi-los."));
-            add(new Questions(60,"Gosto de satisfazer os outros em relação àquilo que me pedem."));
-
-        }
-
-    };
     
-        public Questions(Integer id, String pergunta){
+        public Questions(){
 
-            this.id = id;
-            this.pergunta = pergunta;
+            questionsList.add("Sou bastante autêntico e digo aquilo que penso. ");
+            questionsList.add("Sou introspectivo, penso muito antes de me expor.");
+            questionsList.add("Questões intelectuais profundas não me interessam.");
+            questionsList.add("Sou persistente e não desisto até atingir meus objetivos.");
+            questionsList.add("As pessoas podem contar comigo. Sou bastante estável em minhas ações. ");
+            questionsList.add("Gosto de trabalhar com materiais, com as mãos, objetos e instrumentos. ");
+            questionsList.add("Consigo aguentar por um bom tempo uma atividade física ou intelectual. ");
+            questionsList.add("Dou muito valor para o dinheiro.");
+            questionsList.add("Sei utilizar instrumentos e aparelhos eletrônicos.");
+            questionsList.add("Aceito bem regras, limites, ordens e o contexto no qual vivo.");
+
+            questionsList.add("Gosto de observar, analisar e entender o modo como as coisas, a natureza ou as pessoas funcionam. ");
+            questionsList.add("Não sou muito impulsivo, penso antes de agir.");
+            questionsList.add("Áreas como Matemática e Ciências me atraem. Gosto de estudar um assunto a fundo.");
+            questionsList.add("Tenho facilidade para aprender.");
+            questionsList.add("Sou racional para tomar uma decisão ou para decidir o melhor caminho a seguir.");
+            questionsList.add("Sou muito organizado e detalhista.");
+            questionsList.add("Sou capaz de dar respostas precisas sobre datas, valores e descrever bem determinada situação do passado. ");
+            questionsList.add("Tenho um grande senso crítico. As coisas sempre podem ser melhoradas.");
+            questionsList.add("Gosto de tomar minhas próprias decisões, mesmo que sejam diferentes do pensamento da maioria. ");
+            questionsList.add("Tenho boa habilidade lógica ou matemática.");
+
+            questionsList.add("É difícil para mim aceitar regras, limites e algo que me é imposto.");
+            questionsList.add("Para mim, é fácil viver em um ambiente desorganizado e fazer as coisas sem uma lógica. ");
+            questionsList.add("Sou muito sensível. Acredito que possamos construir um mundo melhor.");
+            questionsList.add("Sou uma pessoa idealista. Acredito que possamos construir um mundo melhor.");
+            questionsList.add("Gosto de criar, inventar, idealizar e fantasiar.");
+            questionsList.add("Gosto de causar impacto, provocando, entretendo, seduzindo, encantando as pessoas. ");
+            questionsList.add("Gosto de descobrir novas possibilidades para as coisas e situações da vida.");
+            questionsList.add("Muitas vezes, não penso muito antes de tomar uma decisão, nem meço as consequências.");
+            questionsList.add("Gosto de me expressar, de representar ou de me mostrar em público. ");
+            questionsList.add("Responsabilizo-me por minhas atitudes e decisões.");
+
+            questionsList.add("Gosto de liderar, de organizar ou de mobilizar um grupo em torno de uma causa ou ação. ");
+            questionsList.add("Gosto de ajudar e colaborar com os outros. Sou leal e benevolente. ");
+            questionsList.add("Uso a emoção e a sensibilidade para tomar decisões.");
+            questionsList.add("Em geral, sou uma pessoa agradável e fácil de lidar.");
+            questionsList.add("Sou idealista e quero mais da vida.");
+            questionsList.add("Questões éticas, de justiça ou bem-estar comum são importantes para mim. ");
+            questionsList.add("Sou responsável. Podem contar comigo porque sempre correspondo ao esperado.");
+            questionsList.add("Costumo me dedicar a pessoas ou a causas, “vestindo a camisa” de uma entidade, de um grupo ou de um clube que eu acredite.");
+            questionsList.add("No geral, me relaciono bem com os outros. Tenho facilidade para dialogar.");
+            questionsList.add("Gosto de ajudar, ensinar, curar e, de alguma forma, quero ser importante para os outros.");
+
+            questionsList.add("Sou ﬁrme e conﬁante em minhas decisões e não preciso de palpites. ");
+            questionsList.add("Muitas vezes consigo convencer as pessoas sobre determinado ponto de vista. ");
+            questionsList.add("Sou uma pessoa motivada, incentivada, otimista.");
+            questionsList.add("Falo muito. Não desisto facilmente de uma argumentação.");
+            questionsList.add("Dou muito valor aos bens materiais. Gosto de comprar e, às vezes, até de demonstrar aos outros minhas conquistas, meus bens materiais, minha sabedoria e meus talentos.");
+            questionsList.add("Tomo decisões rapidamente. Ajo sem reﬂetir muito. Chego a ser impulsivo.");
+            questionsList.add("Gosto de estar no meio das pessoas, especialmente para mostrar a elas o modo como penso.");
+            questionsList.add("Gosto muito de aventuras, de me arriscar e de desaﬁos.");
+            questionsList.add("Estou sempre procurando novas oportunidades, novos caminhos a seguir.");
+            questionsList.add("Preﬁro ser o dono de um negócio do que trabalhar para alguém. ");
+
+            questionsList.add("Aceito bem as regras e o contexto no qual vivo. Gosto de limites claros.");
+            questionsList.add("Sou introvertido, me resguardo e não me exponho muito.");
+            questionsList.add("Cuido de minhas coisas e gosto de conservá-las.");
+            questionsList.add("Sou bem calmo e tenho bom equilíbrio emocional.");
+            questionsList.add("Preﬁro ter um plano de ação, uma rotina deﬁnida de trabalho.");
+            questionsList.add("Sou um tipo de pessoa estável, conﬁável, adequada à realidade.");
+            questionsList.add("Respeito a ética, guardo segredos, cumpro com o combinado.");
+            questionsList.add("Sou muito organizado e detalhista nas coisas. Eﬁcácia é muito importante para mim.");
+            questionsList.add("Eu vou até o ﬁm em meus objetivos. Não desisto até atingi-los.");
+            questionsList.add("Gosto de satisfazer os outros em relação àquilo que me pedem.");
+
 
         }
 
-        public String getPergunta(){ return this.pergunta; }
+        public List<String> getQuestionsList(){
+            return questionsList;
+            }
 
     }
 
--- app/src/main/java/com/example/xurebinhaquiz/QuizActivity.java
+++ app/src/main/java/com/example/xurebinhaquiz/QuizActivity.java
@@ -10,8 +10,13 @@ import android.widget.RadioButton;
 import android.widget.RadioGroup;
 import android.widget.TextView;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class QuizActivity extends AppCompatActivity {
 
+    Questions perguntas = new Questions();
+    int ordemPergunta = 0;
     TextView pergunta;
     RadioButton rbResposta1, rbResposta2, rbResposta3, rbResposta4;
     int pontosR = 0;
@@ -21,31 +26,92 @@ public class QuizActivity extends AppCompatActivity {
     int pontosE = 0;
     int pontosC = 0;
 
+    List perguntaRespondida = new ArrayList();
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_quiz);
         getSupportActionBar().hide();
 
-        pergunta = (TextView) findViewById(R.id.pergunta);
+        pergunta = (TextView)findViewById(R.id.pergunta);
+
+
+        pergunta.setText(perguntas.getQuestionsList().get(ordemPergunta));
+
+
         rbResposta1 = (RadioButton) findViewById(R.id.rbResposta1);
         rbResposta2 = (RadioButton) findViewById(R.id.rbResposta2);
         rbResposta3 = (RadioButton) findViewById(R.id.rbResposta3);
         rbResposta4 = (RadioButton) findViewById(R.id.rbResposta4);
+
+        perguntaRespondida.add(ordemPergunta);
+
     }
 
     public void btnResponderOnClick(View v) {
 
-
-        if (rbResposta1.isChecked()) {
-            pontosR +=1;
-        } else if (rbResposta2.isChecked()) {
-            pontosR +=2;
-        } else if (rbResposta3.isChecked()) {
-            pontosR +=3;
-        } else if (rbResposta4.isChecked()) {
-            pontosR +=4;
-        }
+        while (ordemPergunta<10){
+            if (rbResposta1.isChecked()) {
+                pontosR += 1;
+            } else if (rbResposta2.isChecked()) {
+                pontosR += 2;
+            } else if (rbResposta3.isChecked()) {
+                pontosR += 3;
+            } else if (rbResposta4.isChecked()) {
+                pontosR += 4;
+            }}
+        while (ordemPergunta<20){
+            if (rbResposta1.isChecked()) {
+                pontosI += 1;
+            } else if (rbResposta2.isChecked()) {
+                pontosI += 2;
+            } else if (rbResposta3.isChecked()) {
+                pontosI += 3;
+            } else if (rbResposta4.isChecked()) {
+                pontosI += 4;
+            }}
+        while (ordemPergunta<30){
+            if (rbResposta1.isChecked()) {
+                pontosA += 1;
+            } else if (rbResposta2.isChecked()) {
+                pontosA += 2;
+            } else if (rbResposta3.isChecked()) {
+                pontosA += 3;
+            } else if (rbResposta4.isChecked()) {
+                pontosA += 4;
+            }}
+        while (ordemPergunta<40){
+            if (rbResposta1.isChecked()) {
+                pontosS += 1;
+            } else if (rbResposta2.isChecked()) {
+                pontosS += 2;
+            } else if (rbResposta3.isChecked()) {
+                pontosS += 3;
+            } else if (rbResposta4.isChecked()) {
+                pontosS += 4;
+            }}
+        while (ordemPergunta<50){
+            if (rbResposta1.isChecked()) {
+                pontosE += 1;
+            } else if (rbResposta2.isChecked()) {
+                pontosE += 2;
+            } else if (rbResposta3.isChecked()) {
+                pontosE += 3;
+            } else if (rbResposta4.isChecked()) {
+                pontosE += 4;
+            }}
+        while (ordemPergunta<60){
+            if (rbResposta1.isChecked()) {
+                pontosC += 1;
+            } else if (rbResposta2.isChecked()) {
+                pontosC += 2;
+            } else if (rbResposta3.isChecked()) {
+                pontosC += 3;
+            } else if (rbResposta4.isChecked()) {
+                pontosC += 4;
+            }}
 
 
         Intent intent = new Intent(this, RespostaActivity.class);
@@ -56,8 +122,17 @@ public class QuizActivity extends AppCompatActivity {
     @Override
     protected void onRestart() {
         super.onRestart();
+        Log.v("PontosR:", String.valueOf(pontosR));
+        Log.v("PontosI:", String.valueOf(pontosI));
+        Log.v("PontosA:", String.valueOf(pontosA));
+        Log.v("PontosS:", String.valueOf(pontosS));
+        Log.v("PontosE:", String.valueOf(pontosE));
+        Log.v("PontosC:", String.valueOf(pontosC));
+
+        ordemPergunta++;
+        pergunta.setText(perguntas.getQuestionsList().get(ordemPergunta));
+        }
 
 
 
-    }
 }
