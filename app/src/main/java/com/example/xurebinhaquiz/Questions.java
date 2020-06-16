30920509c8005cdbb9cba0ac54a15e2f8ad9e329
xmacedo <felipe.x.macedo@gmail.com>
2020-06-16 10:05:40 -0300

    Formating and organize

--- app/src/main/java/com/example/xurebinhaquiz/Questions.java
+++ app/src/main/java/com/example/xurebinhaquiz/Questions.java
@@ -5,79 +5,78 @@ import java.util.List;
 
 public class Questions {
 
-    List<String> questionsList = new ArrayList<>();
+    private List<String> questionsList = new ArrayList<>();
 
-        public Questions(){
+    public Questions() {
 
-            questionsList.add("Sou bastante autêntico e digo aquilo que penso. ");
-            questionsList.add("Sou introspectivo, penso muito antes de me expor.");
-            questionsList.add("Questões intelectuais profundas não me interessam.");
-            questionsList.add("Sou persistente e não desisto até atingir meus objetivos.");
-            questionsList.add("As pessoas podem contar comigo. Sou bastante estável em minhas ações. ");
-            questionsList.add("Gosto de trabalhar com materiais, com as mãos, objetos e instrumentos. ");
-            questionsList.add("Consigo aguentar por um bom tempo uma atividade física ou intelectual. ");
-            questionsList.add("Dou muito valor para o dinheiro.");
-            questionsList.add("Sei utilizar instrumentos e aparelhos eletrônicos.");
-            questionsList.add("Aceito bem regras, limites, ordens e o contexto no qual vivo.");
+        questionsList.add("Sou bastante autêntico e digo aquilo que penso. ");
+        questionsList.add("Sou introspectivo, penso muito antes de me expor.");
+        questionsList.add("Questões intelectuais profundas não me interessam.");
+        questionsList.add("Sou persistente e não desisto até atingir meus objetivos.");
+        questionsList.add("As pessoas podem contar comigo. Sou bastante estável em minhas ações. ");
+        questionsList.add("Gosto de trabalhar com materiais, com as mãos, objetos e instrumentos. ");
+        questionsList.add("Consigo aguentar por um bom tempo uma atividade física ou intelectual. ");
+        questionsList.add("Dou muito valor para o dinheiro.");
+        questionsList.add("Sei utilizar instrumentos e aparelhos eletrônicos.");
+        questionsList.add("Aceito bem regras, limites, ordens e o contexto no qual vivo.");
 
-            questionsList.add("Gosto de observar, analisar e entender o modo como as coisas, a natureza ou as pessoas funcionam. ");
-            questionsList.add("Não sou muito impulsivo, penso antes de agir.");
-            questionsList.add("Áreas como Matemática e Ciências me atraem. Gosto de estudar um assunto a fundo.");
-            questionsList.add("Tenho facilidade para aprender.");
-            questionsList.add("Sou racional para tomar uma decisão ou para decidir o melhor caminho a seguir.");
-            questionsList.add("Sou muito organizado e detalhista.");
-            questionsList.add("Sou capaz de dar respostas precisas sobre datas, valores e descrever bem determinada situação do passado. ");
-            questionsList.add("Tenho um grande senso crítico. As coisas sempre podem ser melhoradas.");
-            questionsList.add("Gosto de tomar minhas próprias decisões, mesmo que sejam diferentes do pensamento da maioria. ");
-            questionsList.add("Tenho boa habilidade lógica ou matemática.");
+        questionsList.add("Gosto de observar, analisar e entender o modo como as coisas, a natureza ou as pessoas funcionam. ");
+        questionsList.add("Não sou muito impulsivo, penso antes de agir.");
+        questionsList.add("Áreas como Matemática e Ciências me atraem. Gosto de estudar um assunto a fundo.");
+        questionsList.add("Tenho facilidade para aprender.");
+        questionsList.add("Sou racional para tomar uma decisão ou para decidir o melhor caminho a seguir.");
+        questionsList.add("Sou muito organizado e detalhista.");
+        questionsList.add("Sou capaz de dar respostas precisas sobre datas, valores e descrever bem determinada situação do passado. ");
+        questionsList.add("Tenho um grande senso crítico. As coisas sempre podem ser melhoradas.");
+        questionsList.add("Gosto de tomar minhas próprias decisões, mesmo que sejam diferentes do pensamento da maioria. ");
+        questionsList.add("Tenho boa habilidade lógica ou matemática.");
 
-            questionsList.add("É difícil para mim aceitar regras, limites e algo que me é imposto.");
-            questionsList.add("Para mim, é fácil viver em um ambiente desorganizado e fazer as coisas sem uma lógica. ");
-            questionsList.add("Sou muito sensível. Acredito que possamos construir um mundo melhor.");
-            questionsList.add("Sou uma pessoa idealista. Acredito que possamos construir um mundo melhor.");
-            questionsList.add("Gosto de criar, inventar, idealizar e fantasiar.");
-            questionsList.add("Gosto de causar impacto, provocando, entretendo, seduzindo, encantando as pessoas. ");
-            questionsList.add("Gosto de descobrir novas possibilidades para as coisas e situações da vida.");
-            questionsList.add("Muitas vezes, não penso muito antes de tomar uma decisão, nem meço as consequências.");
-            questionsList.add("Gosto de me expressar, de representar ou de me mostrar em público. ");
-            questionsList.add("Responsabilizo-me por minhas atitudes e decisões.");
+        questionsList.add("É difícil para mim aceitar regras, limites e algo que me é imposto.");
+        questionsList.add("Para mim, é fácil viver em um ambiente desorganizado e fazer as coisas sem uma lógica. ");
+        questionsList.add("Sou muito sensível. Acredito que possamos construir um mundo melhor.");
+        questionsList.add("Sou uma pessoa idealista. Acredito que possamos construir um mundo melhor.");
+        questionsList.add("Gosto de criar, inventar, idealizar e fantasiar.");
+        questionsList.add("Gosto de causar impacto, provocando, entretendo, seduzindo, encantando as pessoas. ");
+        questionsList.add("Gosto de descobrir novas possibilidades para as coisas e situações da vida.");
+        questionsList.add("Muitas vezes, não penso muito antes de tomar uma decisão, nem meço as consequências.");
+        questionsList.add("Gosto de me expressar, de representar ou de me mostrar em público. ");
+        questionsList.add("Responsabilizo-me por minhas atitudes e decisões.");
 
-            questionsList.add("Gosto de liderar, de organizar ou de mobilizar um grupo em torno de uma causa ou ação. ");
-            questionsList.add("Gosto de ajudar e colaborar com os outros. Sou leal e benevolente. ");
-            questionsList.add("Uso a emoção e a sensibilidade para tomar decisões.");
-            questionsList.add("Em geral, sou uma pessoa agradável e fácil de lidar.");
-            questionsList.add("Sou idealista e quero mais da vida.");
-            questionsList.add("Questões éticas, de justiça ou bem-estar comum são importantes para mim. ");
-            questionsList.add("Sou responsável. Podem contar comigo porque sempre correspondo ao esperado.");
-            questionsList.add("Costumo me dedicar a pessoas ou a causas, “vestindo a camisa” de uma entidade, de um grupo ou de um clube que eu acredite.");
-            questionsList.add("No geral, me relaciono bem com os outros. Tenho facilidade para dialogar.");
-            questionsList.add("Gosto de ajudar, ensinar, curar e, de alguma forma, quero ser importante para os outros.");
+        questionsList.add("Gosto de liderar, de organizar ou de mobilizar um grupo em torno de uma causa ou ação. ");
+        questionsList.add("Gosto de ajudar e colaborar com os outros. Sou leal e benevolente. ");
+        questionsList.add("Uso a emoção e a sensibilidade para tomar decisões.");
+        questionsList.add("Em geral, sou uma pessoa agradável e fácil de lidar.");
+        questionsList.add("Sou idealista e quero mais da vida.");
+        questionsList.add("Questões éticas, de justiça ou bem-estar comum são importantes para mim. ");
+        questionsList.add("Sou responsável. Podem contar comigo porque sempre correspondo ao esperado.");
+        questionsList.add("Costumo me dedicar a pessoas ou a causas, “vestindo a camisa” de uma entidade, de um grupo ou de um clube que eu acredite.");
+        questionsList.add("No geral, me relaciono bem com os outros. Tenho facilidade para dialogar.");
+        questionsList.add("Gosto de ajudar, ensinar, curar e, de alguma forma, quero ser importante para os outros.");
 
-            questionsList.add("Sou ﬁrme e conﬁante em minhas decisões e não preciso de palpites. ");
-            questionsList.add("Muitas vezes consigo convencer as pessoas sobre determinado ponto de vista. ");
-            questionsList.add("Sou uma pessoa motivada, incentivada, otimista.");
-            questionsList.add("Falo muito. Não desisto facilmente de uma argumentação.");
-            questionsList.add("Dou muito valor aos bens materiais. Gosto de comprar e, às vezes, até de demonstrar aos outros minhas conquistas, meus bens materiais, minha sabedoria e meus talentos.");
-            questionsList.add("Tomo decisões rapidamente. Ajo sem reﬂetir muito. Chego a ser impulsivo.");
-            questionsList.add("Gosto de estar no meio das pessoas, especialmente para mostrar a elas o modo como penso.");
-            questionsList.add("Gosto muito de aventuras, de me arriscar e de desaﬁos.");
-            questionsList.add("Estou sempre procurando novas oportunidades, novos caminhos a seguir.");
-            questionsList.add("Preﬁro ser o dono de um negócio do que trabalhar para alguém. ");
+        questionsList.add("Sou ﬁrme e conﬁante em minhas decisões e não preciso de palpites. ");
+        questionsList.add("Muitas vezes consigo convencer as pessoas sobre determinado ponto de vista. ");
+        questionsList.add("Sou uma pessoa motivada, incentivada, otimista.");
+        questionsList.add("Falo muito. Não desisto facilmente de uma argumentação.");
+        questionsList.add("Dou muito valor aos bens materiais. Gosto de comprar e, às vezes, até de demonstrar aos outros minhas conquistas, meus bens materiais, minha sabedoria e meus talentos.");
+        questionsList.add("Tomo decisões rapidamente. Ajo sem reﬂetir muito. Chego a ser impulsivo.");
+        questionsList.add("Gosto de estar no meio das pessoas, especialmente para mostrar a elas o modo como penso.");
+        questionsList.add("Gosto muito de aventuras, de me arriscar e de desaﬁos.");
+        questionsList.add("Estou sempre procurando novas oportunidades, novos caminhos a seguir.");
+        questionsList.add("Preﬁro ser o dono de um negócio do que trabalhar para alguém. ");
 
-            questionsList.add("Aceito bem as regras e o contexto no qual vivo. Gosto de limites claros.");
-            questionsList.add("Sou introvertido, me resguardo e não me exponho muito.");
-            questionsList.add("Cuido de minhas coisas e gosto de conservá-las.");
-            questionsList.add("Sou bem calmo e tenho bom equilíbrio emocional.");
-            questionsList.add("Preﬁro ter um plano de ação, uma rotina deﬁnida de trabalho.");
-            questionsList.add("Sou um tipo de pessoa estável, conﬁável, adequada à realidade.");
-            questionsList.add("Respeito a ética, guardo segredos, cumpro com o combinado.");
-            questionsList.add("Sou muito organizado e detalhista nas coisas. Eﬁcácia é muito importante para mim.");
-            questionsList.add("Eu vou até o ﬁm em meus objetivos. Não desisto até atingi-los.");
-            questionsList.add("Gosto de satisfazer os outros em relação àquilo que me pedem.");
-        }
-            public List<String> getQuestionsList(){
-            return questionsList;
-            }
+        questionsList.add("Aceito bem as regras e o contexto no qual vivo. Gosto de limites claros.");
+        questionsList.add("Sou introvertido, me resguardo e não me exponho muito.");
+        questionsList.add("Cuido de minhas coisas e gosto de conservá-las.");
+        questionsList.add("Sou bem calmo e tenho bom equilíbrio emocional.");
+        questionsList.add("Preﬁro ter um plano de ação, uma rotina deﬁnida de trabalho.");
+        questionsList.add("Sou um tipo de pessoa estável, conﬁável, adequada à realidade.");
+        questionsList.add("Respeito a ética, guardo segredos, cumpro com o combinado.");
+        questionsList.add("Sou muito organizado e detalhista nas coisas. Eﬁcácia é muito importante para mim.");
+        questionsList.add("Eu vou até o ﬁm em meus objetivos. Não desisto até atingi-los.");
+        questionsList.add("Gosto de satisfazer os outros em relação àquilo que me pedem.");
     }
 
-
+    public List<String> getQuestionsList() {
+        return questionsList;
+    }
+}
