19b8fd3b7e402067443a3fb446517fa21448ae21
Léo <leomiranda1995@gmail.com>
2020-06-17 01:18:06 -0300

    Envio dos pontos da tela Quiz para a tela Resultado.

--- app/src/main/java/com/example/xurebinhaquiz/ResultadoActivity.java
+++ app/src/main/java/com/example/xurebinhaquiz/ResultadoActivity.java
@@ -2,6 +2,7 @@ package com.example.xurebinhaquiz;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 import android.widget.TextView;
@@ -9,12 +10,22 @@ import android.widget.TextView;
 public class ResultadoActivity extends AppCompatActivity {
 
     TextView resultadoR,resultadoI,resultadoA,resultadoS,resultadoE,resultadoC;
+    private Double pontosR,pontosI,pontosA,pontosS,pontosE,pontosC;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_resultado);
+
+        Intent intent = getIntent();
+        Bundle resultQuiz = intent.getExtras();
+
+        pontosR = resultQuiz.getDouble("pontosR");
+        pontosI = resultQuiz.getDouble("pontosI");
+        pontosA = resultQuiz.getDouble("pontosA");
+        pontosS = resultQuiz.getDouble("pontosS");
+        pontosE = resultQuiz.getDouble("pontosE");
+        pontosC = resultQuiz.getDouble("pontosC");
 
         resultadoR = (TextView) findViewById(R.id.resultadoR);
         resultadoI = (TextView) findViewById(R.id.resultadoI);
@@ -23,11 +34,11 @@ public class ResultadoActivity extends AppCompatActivity {
         resultadoE = (TextView) findViewById(R.id.resultadoE);
         resultadoC = (TextView) findViewById(R.id.resultadoC);
 
-       // resultadoR.setText("Sua porcentagem Realista é: "+);
-       // resultadoI.setText("Sua porcentagem Investigativa é: "+);
-       // resultadoA.setText("Sua porcentagem Artística é: "+);
-       // resultadoS.setText("Sua porcentagem Social é: "+);
-       // resultadoE.setText("Sua porcentagem Empreendedora é: "+);
-       // resultadoC.setText("Sua porcentagem Convencional é: "+);
+       resultadoR.setText("Sua porcentagem Realista é: " + pontosR);
+       resultadoI.setText("Sua porcentagem Investigativa é: " + pontosI);
+       resultadoA.setText("Sua porcentagem Artística é: " + pontosA);
+       resultadoS.setText("Sua porcentagem Social é: " + pontosS);
+       resultadoE.setText("Sua porcentagem Empreendedora é: " + pontosE);
+       resultadoC.setText("Sua porcentagem Convencional é: " + pontosC);
 }
 }
